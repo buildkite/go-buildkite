7d820b6b5aae5557f1ed75bfdf91021a68e781af
Mark Wolfe <mark@wolfe.id.au>
2015-04-05 11:43:26 +1000

    Added support for buildkite access token api and basic auth.

--- buildkite/auth.go
+++ buildkite/auth.go
@@ -6,6 +6,7 @@
 package buildkite
 
 import (
+	"encoding/base64"
 	"fmt"
 	"net/http"
 )
@@ -32,4 +33,34 @@ func NewTokenConfig(apiToken string) (*TokenAuthTransport, error) {
 		return nil, fmt.Errorf("Invalid token, empty string supplied")
 	}
 	return &TokenAuthTransport{APIToken: apiToken}, nil
+}
+
+// BasicAuthTransport manages injection of the authorization header
+type BasicAuthTransport struct {
+	Username string
+	Password string
+}
+
+// RoundTrip invoked each time a request is made
+func (bat BasicAuthTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Set("Authorization", fmt.Sprintf("Basic %s",
+		base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s",
+			bat.Username, bat.Password)))))
+	return http.DefaultTransport.RoundTrip(req)
+}
+
+// Client builds a new http client.
+func (bat *BasicAuthTransport) Client() *http.Client {
+	return &http.Client{Transport: bat}
+}
+
+// NewBasicConfig configure authentication using the supplied credentials
+func NewBasicConfig(username string, password string) (*BasicAuthTransport, error) {
+	if username == "" {
+		return nil, fmt.Errorf("Invalid username, empty string supplied")
+	}
+	if password == "" {
+		return nil, fmt.Errorf("Invalid password, empty string supplied")
+	}
+	return &BasicAuthTransport{username, password}, nil
 }
