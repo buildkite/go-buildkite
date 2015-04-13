ce6065f3db9d2a67740321bff4f2e6cd194fde11
Mark Wolfe <mark@wolfe.id.au>
2015-04-13 19:21:49 +1000

    Flesh out some of the build related REST calls.

--- buildkite/builds.go
+++ buildkite/builds.go
@@ -69,13 +69,67 @@ type BuildsListOptions struct {
 	ListOptions
 }
 
-// List the organizations for the current user.
+// List the builds for the current user.
 //
 // buildkite API docs: https://buildkite.com/docs/api/organizations#list-organizations
 func (bs *BuildsService) List(opt *BuildsListOptions) ([]Build, *Response, error) {
 	var u string
 
 	u = fmt.Sprintf("v1/builds")
+
+	u, err := addOptions(u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := bs.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	orgs := new([]Build)
+	resp, err := bs.client.Do(req, orgs)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return *orgs, resp, err
+}
+
+// ListByOrg lists the builds within the specified orginisation.
+//
+// buildkite API docs: https://buildkite.com/docs/api/builds#list-builds-for-an-organization
+func (bs *BuildsService) ListByOrg(org string, opt *BuildsListOptions) ([]Build, *Response, error) {
+	var u string
+
+	u = fmt.Sprintf("v1/organizations/%s/builds", org)
+
+	u, err := addOptions(u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := bs.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	orgs := new([]Build)
+	resp, err := bs.client.Do(req, orgs)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return *orgs, resp, err
+}
+
+// ListByProject lists the builds for a project within the specified originisation.
+//
+// buildkite API docs: https://buildkite.com/docs/api/builds#list-builds-for-a-project
+func (bs *BuildsService) ListByProject(org string, project string, opt *BuildsListOptions) ([]Build, *Response, error) {
+	var u string
+
+	u = fmt.Sprintf("v1/organizations/%s/projects/%s/builds", org, project)
 
 	u, err := addOptions(u, opt)
 	if err != nil {
--- buildkite/builds_test.go
+++ buildkite/builds_test.go
@@ -51,3 +51,43 @@ func TestBuildsService_List_by_status(t *testing.T) {
 		t.Errorf("Builds.List returned %+v, want %+v", builds, want)
 	}
 }
+
+func TestBuildsService_ListByOrg(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/organizations/my-great-org/builds", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, `[{"id":"123"},{"id":"1234"}]`)
+	})
+
+	builds, _, err := client.Builds.ListByOrg("my-great-org", nil)
+	if err != nil {
+		t.Errorf("Builds.List returned error: %v", err)
+	}
+
+	want := []Build{{ID: String("123")}, {ID: String("1234")}}
+	if !reflect.DeepEqual(builds, want) {
+		t.Errorf("Builds.List returned %+v, want %+v", builds, want)
+	}
+}
+
+func TestBuildsService_ListByProject(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v1/organizations/my-great-org/projects/sup-keith/builds", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, `[{"id":"123"},{"id":"1234"}]`)
+	})
+
+	builds, _, err := client.Builds.ListByProject("my-great-org", "sup-keith", nil)
+	if err != nil {
+		t.Errorf("Builds.List returned error: %v", err)
+	}
+
+	want := []Build{{ID: String("123")}, {ID: String("1234")}}
+	if !reflect.DeepEqual(builds, want) {
+		t.Errorf("Builds.List returned %+v, want %+v", builds, want)
+	}
+}
