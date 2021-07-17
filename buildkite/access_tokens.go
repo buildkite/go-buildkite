9c1068861595067c9b8a2158a181dee51215e7bc
alam0rt <samlockart@gmail.com>
2021-07-17 11:25:14 +1000

    use service instead of direct client

--- buildkite/access_tokens.go
+++ buildkite/access_tokens.go
@@ -16,20 +16,20 @@ type Scope string
 // GetToken gets the current token which was used to authenticate the request
 //
 // buildkite API docs: https://buildkite.com/docs/rest-api/access-token
-func (c *Client) GetToken() (*AccessToken, *Response, error) {
+func (ats *AccessTokensService) GetToken() (*AccessToken, *Response, error) {
 
 	var u string
 
 	u = fmt.Sprintf("v2/access-token")
 
-	req, err := c.NewRequest("GET", u, nil)
+	req, err := ats.client.NewRequest("GET", u, nil)
 	if err != nil {
 		return nil, nil, err
 	}
 
 	var accessToken *AccessToken
 	fmt.Print(req.Body)
-	resp, err := c.Do(req, accessToken)
+	resp, err := ats.client.Do(req, accessToken)
 	if err != nil {
 		return nil, resp, err
 	}
