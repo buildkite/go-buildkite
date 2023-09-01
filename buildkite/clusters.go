bdefdc7bb7989e29c0660910675b4105ebfd0103
James Sammut <juma2791@gmail.com>
2023-09-01 13:28:13 +1000

    Get, Create, Update, Delete ops. Dedicated structs

--- buildkite/clusters.go
+++ buildkite/clusters.go
@@ -1,13 +1,31 @@
 package buildkite
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
-// FlakyTestsService handles communication with cluster related
+// ClustersService handles communication with cluster related
 // methods of the Buildkite API.
 //
 // Buildkite API docs: https://buildkite.com/docs/apis/rest-api/clusters#clusters
 type ClustersService struct {
 	client *Client
+}
+
+type ClusterCreate struct {
+	Name            *string         `json:"name,omitempty" yaml:"name,omitempty"`
+	Description     *string         `json:"description,omitempty" yaml:"description,omitempty"`
+	Emoji           *string         `json:"emoji,omitempty" yaml:"emoji,omitempty"`
+	Color           *string         `json:"color,omitempty" yaml:"color,omitempty"`
+}
+
+type ClusterUpdate struct {
+	Name            *string         `json:"name,omitempty" yaml:"name,omitempty"`
+	Description     *string         `json:"description,omitempty" yaml:"description,omitempty"`
+	Emoji           *string         `json:"emoji,omitempty" yaml:"emoji,omitempty"`
+	Color           *string         `json:"color,omitempty" yaml:"color,omitempty"`
+	DefaultQueueID  *string         `json:"default_queue_id,omitempty" yaml:"default_queue_id,omitempty"`
 }
 
 type Cluster struct {
@@ -39,7 +57,7 @@ type ClustersListOptions struct {
 	ListOptions
 }
 
-func (cs *ClustersService) List(org, slug string, opt *ClustersListOptions) ([]Cluster, *Response, error) {
+func (cs *ClustersService) List(org string, opt *ClustersListOptions) ([]Cluster, *Response, error) {
 
 	u := fmt.Sprintf("v2/organizations/%s/clusters", org)
 
@@ -64,4 +82,86 @@ func (cs *ClustersService) List(org, slug string, opt *ClustersListOptions) ([]C
 	}
 
 	return *clusters, resp, err
+}
+
+func (cs *ClustersService) Get(org, id string) (*Cluster, *Response, error) {
+
+	u := fmt.Sprintf("v2/organizations/%s/clusters/%s", org, id)
+
+	req, err := cs.client.NewRequest("GET", u, nil)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	cluster := new(Cluster)
+
+	resp, err := cs.client.Do(req, cluster)
+
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return cluster, resp, err
+}
+
+func (cs *ClustersService) Create(org string, cc *ClusterCreate) (*Cluster, *Response, error) {
+
+	if cc == nil {
+		return nil, nil, errors.New("ClusterCreate struct instance must not be nil")
+	}
+
+	u := fmt.Sprintf("v2/organizations/%s/clusters", org)
+
+	req, err := cs.client.NewRequest("POST", u, cc)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	cluster := new(Cluster)
+
+	resp, err := cs.client.Do(req, cluster)
+
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return cluster, resp, err
+}
+
+func (cs *ClustersService) Update(org, id string, cu *ClusterUpdate) (*Response, error) {
+
+	if cu == nil {
+		return nil, errors.New("ClusterUpdate struct instance must not be nil")
+	}
+
+	u := fmt.Sprintf("v2/organizations/%s/clusters/%s", org, id)
+
+	req, err := cs.client.NewRequest("PATCH", u, cu)
+
+	if err != nil {
+		return nil, nil
+	}
+
+	resp, err := cs.client.Do(req, cu)
+
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, err
+}
+
+func (cs *ClustersService) Delete(org, id string) (*Response, error) {
+
+	u := fmt.Sprintf("v2/organizations/%s/clusters/%s", org, id)
+
+	req, err := cs.client.NewRequest("DELETE", u, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cs.client.Do(req, nil)
 }
