4867569216f608e3ea21c24eed19b0657ea99308
James Sammut <juma2791@gmail.com>
2023-09-04 10:25:43 +1000

    Initial service impl, ClusterQueue struct, List

---
+++ buildkite/cluster_queues.go
@@ -0,0 +1,60 @@
+package buildkite
+
+import (
+	"fmt"
+)
+
+// ClusterQueuesService handles communication with cluster queue related
+// methods of the Buildkite API.
+//
+// Buildkite API docs: https://buildkite.com/docs/apis/rest-api/clusters#cluster-queues
+type ClusterQueuesService struct {
+	client *Client
+}
+
+type ClusterQueue struct {
+	ID                 *string         `json:"id,omitempty" yaml:"id,omitempty"`
+	GraphQLID          *string         `json:"graphql_id,omitempty" yaml:"graphql_id,omitempty"`
+	Key                *string         `json:"key,omitempty" yaml:"key,omitempty"`
+	Description        *string         `json:"description,omitempty" yaml:"description,omitempty"`
+	URL                *string         `json:"url,omitempty" yaml:"url,omitempty"`
+	WebURL             *string         `json:"web_url,omitempty" yaml:"web_url,omitempty"`
+	ClusterURL         *string         `json:"cluster_url,omitempty" yaml:"cluster_url,omitempty"`
+	DispatchPaused     *bool           `json:"dispatch_paused,omitempty" yaml:"dispatch_paused,omitempty"`
+	DispatchPausedBy   *User           `json:"dispatch_paused_by,omitempty" yaml:"dispatch_paused_by,omitempty"`
+	DispatchPausedAt   *Timestamp      `json:"dispatch_paused_at,omitempty" yaml:"dispatch_paused_at,omitempty"`
+	DispatchPausedNote *string         `json:"dispatch_paused_note,omitempty" yaml:"dispatch_paused_note,omitempty"`
+	CreatedAt          *Timestamp      `json:"created_at,omitempty" yaml:"created_at,omitempty"`
+	CreatedBy          *ClusterCreator `json:"created_by,omitempty" yaml:"created_by,omitempty"`
+}
+
+type ClusterQueuesListOptions struct {
+	ListOptions
+}
+
+func (cqs *ClusterQueuesService) List(org, clusterID string,  opt *ClusterQueuesListOptions) ([]ClusterQueue, *Response, error) {
+
+	u := fmt.Sprintf("v2/organizations/%s/clusters/%s/queues", org, clusterID)
+
+	u, err := addOptions(u, opt)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := cqs.client.NewRequest("GET", u, nil)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	clusterQueues := new([]ClusterQueue)
+
+	resp, err := cqs.client.Do(req, clusterQueues)
+
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return *clusterQueues, resp, err
+}
