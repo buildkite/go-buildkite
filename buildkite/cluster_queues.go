b01902fc84d95c4c1e0964f14814d8305dca2f38
James Sammut <juma2791@gmail.com>
2023-09-06 08:28:55 +1000

    Additional Cluster queue functions

--- buildkite/cluster_queues.go
+++ buildkite/cluster_queues.go
@@ -1,6 +1,7 @@
 package buildkite
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -13,26 +14,39 @@ type ClusterQueuesService struct {
 }
 
 type ClusterQueue struct {
-	ID                 *string         `json:"id,omitempty" yaml:"id,omitempty"`
-	GraphQLID          *string         `json:"graphql_id,omitempty" yaml:"graphql_id,omitempty"`
-	Key                *string         `json:"key,omitempty" yaml:"key,omitempty"`
-	Description        *string         `json:"description,omitempty" yaml:"description,omitempty"`
-	URL                *string         `json:"url,omitempty" yaml:"url,omitempty"`
-	WebURL             *string         `json:"web_url,omitempty" yaml:"web_url,omitempty"`
-	ClusterURL         *string         `json:"cluster_url,omitempty" yaml:"cluster_url,omitempty"`
-	DispatchPaused     *bool           `json:"dispatch_paused,omitempty" yaml:"dispatch_paused,omitempty"`
-	DispatchPausedBy   *User           `json:"dispatch_paused_by,omitempty" yaml:"dispatch_paused_by,omitempty"`
-	DispatchPausedAt   *Timestamp      `json:"dispatch_paused_at,omitempty" yaml:"dispatch_paused_at,omitempty"`
-	DispatchPausedNote *string         `json:"dispatch_paused_note,omitempty" yaml:"dispatch_paused_note,omitempty"`
-	CreatedAt          *Timestamp      `json:"created_at,omitempty" yaml:"created_at,omitempty"`
-	CreatedBy          *ClusterCreator `json:"created_by,omitempty" yaml:"created_by,omitempty"`
+	ID                 string         `json:"id,omitempty" yaml:"id,omitempty"`
+	GraphQLID          string         `json:"graphql_id,omitempty" yaml:"graphql_id,omitempty"`
+	Key                string         `json:"key,omitempty" yaml:"key,omitempty"`
+	Description        *string        `json:"description,omitempty" yaml:"description,omitempty"`
+	URL                string         `json:"url,omitempty" yaml:"url,omitempty"`
+	WebURL             string         `json:"web_url,omitempty" yaml:"web_url,omitempty"`
+	ClusterURL         string         `json:"cluster_url,omitempty" yaml:"cluster_url,omitempty"`
+	DispatchPaused     bool           `json:"dispatch_paused,omitempty" yaml:"dispatch_paused,omitempty"`
+	DispatchPausedBy   *User          `json:"dispatch_paused_by,omitempty" yaml:"dispatch_paused_by,omitempty"`
+	DispatchPausedAt   *Timestamp     `json:"dispatch_paused_at,omitempty" yaml:"dispatch_paused_at,omitempty"`
+	DispatchPausedNote *string        `json:"dispatch_paused_note,omitempty" yaml:"dispatch_paused_note,omitempty"`
+	CreatedAt          Timestamp      `json:"created_at,omitempty" yaml:"created_at,omitempty"`
+	CreatedBy          ClusterCreator `json:"created_by,omitempty" yaml:"created_by,omitempty"`
+}
+
+type ClusterQueueCreate struct {
+	Key         string  `json:"key,omitempty" yaml:"key,omitempty"`
+	Description *string `json:"description,omitempty" yaml:"description,omitempty"`
+}
+
+type ClusterQueueUpdate struct {
+	Description *string `json:"description,omitempty" yaml:"description,omitempty"`
+}
+
+type ClusterQueuePause struct {
+	Note *string `json:"note,omitempty" yaml:"note,omitempty"`
 }
 
 type ClusterQueuesListOptions struct {
 	ListOptions
 }
 
-func (cqs *ClusterQueuesService) List(org, clusterID string,  opt *ClusterQueuesListOptions) ([]ClusterQueue, *Response, error) {
+func (cqs *ClusterQueuesService) List(org, clusterID string, opt *ClusterQueuesListOptions) ([]ClusterQueue, *Response, error) {
 
 	u := fmt.Sprintf("v2/organizations/%s/clusters/%s/queues", org, clusterID)
 
@@ -48,13 +62,127 @@ func (cqs *ClusterQueuesService) List(org, clusterID string,  opt *ClusterQueues
 		return nil, nil, err
 	}
 
-	clusterQueues := new([]ClusterQueue)
+	queues := new([]ClusterQueue)
 
-	resp, err := cqs.client.Do(req, clusterQueues)
+	resp, err := cqs.client.Do(req, queues)
 
 	if err != nil {
 		return nil, resp, err
 	}
 
-	return *clusterQueues, resp, err
+	return *queues, resp, err
+}
+
+func (cqs *ClusterQueuesService) Get(org, clusterID, queueID string) (*ClusterQueue, *Response, error) {
+
+	u := fmt.Sprintf("v2/organizations/%s/clusters/%s/queues/%s", org, clusterID, queueID)
+
+	req, err := cqs.client.NewRequest("GET", u, nil)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	queue := new(ClusterQueue)
+
+	resp, err := cqs.client.Do(req, queue)
+
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return queue, resp, err
+}
+
+func (cqs *ClusterQueuesService) Create(org, clusterID string, qc *ClusterQueueCreate) (*ClusterQueue, *Response, error) {
+
+	if qc == nil {
+		return nil, nil, errors.New("ClusterQueueCreate struct instance must not be nil")
+	}
+
+	u := fmt.Sprintf("v2/organizations/%s/clusters/%s/queues", org, clusterID)
+
+	req, err := cqs.client.NewRequest("POST", u, qc)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	queue := new(ClusterQueue)
+
+	resp, err := cqs.client.Do(req, queue)
+
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return queue, resp, err
+}
+
+func (cqs *ClusterQueuesService) Update(org, clusterID, queueID string, qu *ClusterQueueUpdate) (*ClusterQueue, *Response, error) {
+
+	if qu == nil {
+		return nil, nil, errors.New("ClusterQueueUpdate struct instance must not be nil")
+	}
+
+	u := fmt.Sprintf("v2/organizations/%s/clusters/%s/queues/%s", org, clusterID, queueID)
+
+	req, err := cqs.client.NewRequest("POST", u, qu)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	queue := new(ClusterQueue)
+
+	resp, err := cqs.client.Do(req, queue)
+
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return queue, resp, err
+}
+
+func (cqs *ClusterQueuesService) Delete(org, clusterID, queueID string) (*Response, error) {
+
+	u := fmt.Sprintf("v2/organizations/%s/clusters/%s/queues/%s", org, clusterID, queueID)
+
+	req, err := cqs.client.NewRequest("DELETE", u, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cqs.client.Do(req, nil)
+}
+
+func (cqs *ClusterQueuesService) Pause(org, clusterID, queueID string, qp *ClusterQueuePause) (*Response, error) {
+
+	if qp == nil {
+		return nil, errors.New("ClusterQueuePause struct instance must not be nil")
+	}
+
+	u := fmt.Sprintf("v2/organizations/%s/clusters/%s/queues/%s/pause_dispatch", org, clusterID, queueID)
+
+	req, err := cqs.client.NewRequest("POST", u, qp)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cqs.client.Do(req, nil)
+}
+
+func (cqs *ClusterQueuesService) Resume(org, clusterID, queueID string) (*Response, error) {
+
+	u := fmt.Sprintf("v2/organizations/%s/clusters/%s/queues/%s/resume_dispatch", org, clusterID, queueID)
+
+	req, err := cqs.client.NewRequest("POST", u, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cqs.client.Do(req, nil)
 }
