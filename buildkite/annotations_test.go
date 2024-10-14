18b6976d6e59ffc1b11538b39d09e3230c610aa2
Ben Moskovitz <ben.m@buildkite.com>
2024-10-14 16:03:03 +1100

    Remove needless pointers from annotations

--- buildkite/annotations_test.go
+++ buildkite/annotations_test.go
@@ -44,18 +44,18 @@ func TestAnnotationsService_ListByBuild(t *testing.T) {
 
 	want := []Annotation{
 		{
-			ID:        String("de0d4ab5-6360-467a-a34b-e5ef5db5320d"),
-			Context:   String("default"),
-			Style:     String("info"),
-			BodyHTML:  String("<h1>My Markdown Heading</h1>\n<img src=\"artifact://indy.png\" alt=\"Belongs in a museum\" height=250 />"),
+			ID:        "de0d4ab5-6360-467a-a34b-e5ef5db5320d",
+			Context:   "default",
+			Style:     "info",
+			BodyHTML:  "<h1>My Markdown Heading</h1>\n<img src=\"artifact://indy.png\" alt=\"Belongs in a museum\" height=250 />",
 			CreatedAt: NewTimestamp(time.Date(2019, 4, 9, 18, 7, 15, 775000000, time.UTC)),
 			UpdatedAt: NewTimestamp(time.Date(2019, 8, 6, 20, 58, 49, 396000000, time.UTC)),
 		},
 		{
-			ID:        String("5b3ceff6-78cb-4fe9-88ae-51be5f145977"),
-			Context:   String("coverage"),
-			Style:     String("info"),
-			BodyHTML:  String("Read the <a href=\"artifact://coverage/index.html\">uploaded coverage report</a>"),
+			ID:        "5b3ceff6-78cb-4fe9-88ae-51be5f145977",
+			Context:   "coverage",
+			Style:     "info",
+			BodyHTML:  "Read the <a href=\"artifact://coverage/index.html\">uploaded coverage report</a>",
 			CreatedAt: NewTimestamp(time.Date(2019, 4, 9, 18, 7, 16, 320000000, time.UTC)),
 			UpdatedAt: NewTimestamp(time.Date(2019, 4, 9, 18, 7, 16, 320000000, time.UTC)),
 		},
@@ -71,15 +71,15 @@ func TestAnnotationsService_Create(t *testing.T) {
 	server, client, teardown := newMockServerAndClient(t)
 	t.Cleanup(teardown)
 
-	input := &AnnotationCreate{
-		Style:   String("info"),
-		Context: String("default"),
-		Body:    String("<h1>My Markdown Heading</h1>\n<p>An example annotation!</p>"),
-		Append:  Bool(false),
+	input := AnnotationCreate{
+		Style:   "info",
+		Context: "default",
+		Body:    "<h1>My Markdown Heading</h1>\n<p>An example annotation!</p>",
+		Append:  false,
 	}
 
 	server.HandleFunc("/v2/organizations/my-great-org/pipelines/my-great-pipeline/builds/10/annotations", func(w http.ResponseWriter, r *http.Request) {
-		v := new(AnnotationCreate)
+		var v AnnotationCreate
 		json.NewDecoder(r.Body).Decode(&v)
 
 		testMethod(t, r, "POST")
@@ -101,7 +101,6 @@ func TestAnnotationsService_Create(t *testing.T) {
 	})
 
 	annotation, _, err := client.Annotations.Create(context.Background(), "my-great-org", "my-great-pipeline", "10", input)
-
 	if err != nil {
 		t.Errorf("TestAnnotations.Create returned error: %v", err)
 	}
@@ -109,11 +108,11 @@ func TestAnnotationsService_Create(t *testing.T) {
 	annotationCreatedAt := must(time.Parse(BuildKiteDateFormat, "2023-08-21T08:50:05.824Z"))
 	annotationUpatedAt := must(time.Parse(BuildKiteDateFormat, "2023-08-21T08:50:05.824Z"))
 
-	want := &Annotation{
-		ID:        String("68aef727-f754-48e1-aad8-5f5da8a9960c"),
-		Context:   String("default"),
-		Style:     String("info"),
-		BodyHTML:  String("<h1>My Markdown Heading</h1>\n<p>An example annotation!</p>"),
+	want := Annotation{
+		ID:        "68aef727-f754-48e1-aad8-5f5da8a9960c",
+		Context:   "default",
+		Style:     "info",
+		BodyHTML:  "<h1>My Markdown Heading</h1>\n<p>An example annotation!</p>",
 		CreatedAt: NewTimestamp(annotationCreatedAt),
 		UpdatedAt: NewTimestamp(annotationUpatedAt),
 	}
