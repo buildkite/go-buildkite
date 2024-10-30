eb4e62df1da942a0b0d6205063ca33bc54969695
Tiffany Lin <tifflin@uber.com>
2024-10-30 19:05:32 +0000

    Expand the Teams service to support additional APIs

--- teams.go
+++ teams.go
@@ -35,6 +35,35 @@ type TeamsListOptions struct {
 	UserID string `url:"user_id,omitempty"`
 }
 
+// CreateTeam represents a request to create a team.
+type CreateTeam struct {
+	Name                      string `json:"name,omitempty"`
+	Description               string `json:"description,omitempty"`
+	Privacy                   string `json:"privacy,omitempty"`
+	IsDefaultTeam             bool   `json:"is_default_team,omitempty"`
+	DefaultMemberRole         string `json:"default_member_role,omitempty"`
+	MembersCanCreatePipelines bool   `json:"members_can_create_pipelines,omitempty"`
+}
+
+// TeamMember represents a member of a team.
+type TeamMember struct {
+	ID        string    `json:"user_id,omitempty"`
+	UserName  string    `json:"user_name,omitempty"`
+	Role      string    `json:"role,omitempty"`
+	CreatedAt Timestamp `json:"created_at,omitempty"`
+}
+
+// CreateTeamMember represents a request to create a team member.
+type CreateTeamMember struct {
+	UserID string `json:"user_id,omitempty"`
+	Role   string `json:"role,omitempty"`
+}
+
+// TeamMembersListOptions specifies the optional parameters to the ListTeamMembers method.
+type TeamMembersListOptions struct {
+	ListOptions
+}
+
 // Get the teams for an org.
 //
 // buildkite API docs: https://buildkite.com/docs/api
@@ -57,4 +86,168 @@ func (ts *TeamsService) List(ctx context.Context, org string, opt *TeamsListOpti
 	}
 
 	return teams, resp, err
+}
+
+// GetTeam gets a team.
+func (ts *TeamsService) GetTeam(ctx context.Context, org string, id string) (Team, error) {
+	u := fmt.Sprintf("v2/organizations/%s/teams/%s", org, id)
+
+	req, err := ts.client.NewRequest(ctx, "GET", u, nil)
+	if err != nil {
+		return Team{}, err
+	}
+
+	var team Team
+	_, err = ts.client.Do(req, &team)
+	if err != nil {
+		return team, err
+	}
+
+	return team, err
+}
+
+// CreateTeam creates a team.
+func (ts *TeamsService) CreateTeam(ctx context.Context, org string, t CreateTeam) (Team, *Response, error) {
+	u := fmt.Sprintf("v2/organizations/%s/teams", org)
+
+	req, err := ts.client.NewRequest(ctx, "POST", u, t)
+	if err != nil {
+		return Team{}, nil, err
+	}
+
+	var team Team
+	resp, err := ts.client.Do(req, &team)
+	if err != nil {
+		return team, nil, err
+	}
+
+	return team, resp, err
+}
+
+// UpdateTeam updates a team.
+func (ts *TeamsService) UpdateTeam(ctx context.Context, org string, id string, t CreateTeam) (Team, *Response, error) {
+	u := fmt.Sprintf("v2/organizations/%s/teams/%s", org, id)
+
+	req, err := ts.client.NewRequest(ctx, "PATCH", u, t)
+	if err != nil {
+		return Team{}, nil, err
+	}
+
+	var team Team
+	resp, err := ts.client.Do(req, &team)
+	if err != nil {
+		return team, nil, err
+	}
+
+	return team, resp, err
+}
+
+// DeleteTeam deletes a team.
+func (ts *TeamsService) DeleteTeam(ctx context.Context, org string, id string) (*Response, error) {
+	u := fmt.Sprintf("v2/organizations/%s/teams/%s", org, id)
+
+	req, err := ts.client.NewRequest(ctx, "DELETE", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := ts.client.Do(req, nil)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, err
+}
+
+// ListTeamMembers lists the members of a team.
+func (ts *TeamsService) ListTeamMembers(ctx context.Context, org string, id string, opt *TeamMembersListOptions) ([]TeamMember, *Response, error) {
+	u := fmt.Sprintf("v2/organizations/%s/teams/%s/members", org, id)
+	u, err := addOptions(u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := ts.client.NewRequest(ctx, "GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var teamMembers []TeamMember
+	resp, err := ts.client.Do(req, &teamMembers)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return teamMembers, resp, err
+}
+
+// GetTeamMember gets a team member.
+func (ts *TeamsService) GetTeamMember(ctx context.Context, org string, teamID string, userID string) (TeamMember, error) {
+	u := fmt.Sprintf("v2/organizations/%s/teams/%s/members/%s", org, teamID, userID)
+
+	req, err := ts.client.NewRequest(ctx, "GET", u, nil)
+	if err != nil {
+		return TeamMember{}, err
+	}
+
+	var teamMember TeamMember
+	_, err = ts.client.Do(req, &teamMember)
+	if err != nil {
+		return teamMember, err
+	}
+
+	return teamMember, err
+}
+
+// CreateTeamMember creates a team member.
+func (ts *TeamsService) CreateTeamMember(ctx context.Context, org string, teamID string, t CreateTeamMember) (TeamMember, *Response, error) {
+	u := fmt.Sprintf("v2/organizations/%s/teams/%s/members", org, teamID)
+
+	req, err := ts.client.NewRequest(ctx, "POST", u, t)
+	if err != nil {
+		return TeamMember{}, nil, err
+	}
+
+	var teamMember TeamMember
+	resp, err := ts.client.Do(req, &teamMember)
+	if err != nil {
+		return teamMember, resp, err
+	}
+
+	return teamMember, resp, err
+}
+
+// UpdateTeamMember updates a team member.
+func (ts *TeamsService) UpdateTeamMember(ctx context.Context, org string, teamID string, userID string, role string) (TeamMember, *Response, error) {
+	u := fmt.Sprintf("v2/organizations/%s/teams/%s/members/%s", org, teamID, userID)
+
+	req, err := ts.client.NewRequest(ctx, "PATCH", u, map[string]string{"role": role})
+	if err != nil {
+		return TeamMember{}, nil, err
+	}
+
+	var teamMember TeamMember
+	resp, err := ts.client.Do(req, &teamMember)
+	if err != nil {
+		return teamMember, resp, err
+	}
+
+	return teamMember, resp, err
+}
+
+// DeleteTeamMember deletes a team member.
+func (ts *TeamsService) DeleteTeamMember(ctx context.Context, org string, teamID string, userID string) (*Response, error) {
+	u := fmt.Sprintf("v2/organizations/%s/teams/%s/members/%s", org, teamID, userID)
+
+	req, err := ts.client.NewRequest(ctx, "DELETE", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := ts.client.Do(req, nil)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, err
 }
--- teams_test.go
+++ teams_test.go
@@ -56,3 +56,218 @@ func TestTeamsService_ListForUser(t *testing.T) {
 		t.Errorf("Teams.List diff: (-got +want)\n%s", diff)
 	}
 }
+
+func TestTeamsService_GetTeam(t *testing.T) {
+	t.Parallel()
+
+	server, client, teardown := newMockServerAndClient(t)
+	t.Cleanup(teardown)
+
+	server.HandleFunc("/v2/organizations/my-great-org/teams/123", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, `{"id":"123"}`)
+	})
+
+	team, err := client.Teams.GetTeam(context.Background(), "my-great-org", "123")
+	if err != nil {
+		t.Errorf("Teams.GetTeam returned error: %v", err)
+	}
+
+	want := Team{ID: "123"}
+	if diff := cmp.Diff(team, want); diff != "" {
+		t.Errorf("Teams.GetTeam diff: (-got +want)\n%s", diff)
+	}
+}
+
+func TestTeamsService_CreateTeam(t *testing.T) {
+	t.Parallel()
+
+	server, client, teardown := newMockServerAndClient(t)
+	t.Cleanup(teardown)
+
+	server.HandleFunc("/v2/organizations/my-great-org/teams", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		fmt.Fprint(w, `{"id":"123"}`)
+	})
+
+	team, _, err := client.Teams.CreateTeam(context.Background(), "my-great-org", CreateTeam{})
+	if err != nil {
+		t.Errorf("Teams.CreateTeam returned error: %v", err)
+	}
+
+	want := Team{ID: "123"}
+	if diff := cmp.Diff(team, want); diff != "" {
+		t.Errorf("Teams.CreateTeam diff: (-got +want)\n%s", diff)
+	}
+}
+
+func TestTeamsService_UpdateTeam(t *testing.T) {
+	t.Parallel()
+
+	server, client, teardown := newMockServerAndClient(t)
+	t.Cleanup(teardown)
+
+	server.HandleFunc("/v2/organizations/my-great-org/teams/123", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "PATCH")
+		fmt.Fprint(w, `{"id":"123"}`)
+	})
+
+	team, _, err := client.Teams.UpdateTeam(context.Background(), "my-great-org", "123", CreateTeam{})
+	if err != nil {
+		t.Errorf("Teams.UpdateTeam returned error: %v", err)
+	}
+
+	want := Team{ID: "123"}
+	if diff := cmp.Diff(team, want); diff != "" {
+		t.Errorf("Teams.UpdateTeam diff: (-got +want)\n%s", diff)
+	}
+}
+
+func TestTeamsService_DeleteTeam(t *testing.T) {
+	t.Parallel()
+
+	server, client, teardown := newMockServerAndClient(t)
+	t.Cleanup(teardown)
+
+	server.HandleFunc("/v2/organizations/my-great-org/teams/123", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "DELETE")
+	})
+
+	_, err := client.Teams.DeleteTeam(context.Background(), "my-great-org", "123")
+	if err != nil {
+		t.Errorf("Teams.DeleteTeam returned error: %v", err)
+	}
+}
+
+func TestTeamsService_ListTeamMembers(t *testing.T) {
+	t.Parallel()
+
+	server, client, teardown := newMockServerAndClient(t)
+	t.Cleanup(teardown)
+
+	server.HandleFunc("/v2/organizations/my-great-org/teams/123/members", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, `[{"user_id":"123"}]`)
+	})
+
+	members, _, err := client.Teams.ListTeamMembers(context.Background(), "my-great-org", "123", nil)
+	if err != nil {
+		t.Errorf("Teams.ListTeamMembers returned error: %v", err)
+	}
+
+	want := []TeamMember{{ID: "123"}}
+	if diff := cmp.Diff(members, want); diff != "" {
+		t.Errorf("Teams.ListTeamMembers diff: (-got +want)\n%s", diff)
+	}
+}
+
+func TestTeamsService_ListTeamMembersPaginated(t *testing.T) {
+	t.Parallel()
+
+	server, client, teardown := newMockServerAndClient(t)
+	t.Cleanup(teardown)
+
+	server.HandleFunc("/v2/organizations/my-great-org/teams/123/members", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testFormValues(t, r, values{
+			"page": "2",
+		})
+		fmt.Fprint(w, `[{"user_id":"123"},{"user_id":"456"}]`)
+	})
+
+	opt := &TeamMembersListOptions{
+		ListOptions: ListOptions{Page: 2},
+	}
+	members, _, err := client.Teams.ListTeamMembers(context.Background(), "my-great-org", "123", opt)
+	if err != nil {
+		t.Errorf("Teams.ListTeamMembers returned error: %v", err)
+	}
+
+	want := []TeamMember{{ID: "123"}, {ID: "456"}}
+	if diff := cmp.Diff(members, want); diff != "" {
+		t.Errorf("Teams.ListTeamMembers diff: (-got +want)\n%s", diff)
+	}
+}
+
+func TestTeamsService_GetTeamMember(t *testing.T) {
+	t.Parallel()
+
+	server, client, teardown := newMockServerAndClient(t)
+	t.Cleanup(teardown)
+
+	server.HandleFunc("/v2/organizations/my-great-org/teams/123/members/456", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, `{"user_id":"456"}`)
+	})
+
+	member, err := client.Teams.GetTeamMember(context.Background(), "my-great-org", "123", "456")
+	if err != nil {
+		t.Errorf("Teams.GetTeamMember returned error: %v", err)
+	}
+
+	want := TeamMember{ID: "456"}
+	if diff := cmp.Diff(member, want); diff != "" {
+		t.Errorf("Teams.GetTeamMember diff: (-got +want)\n%s", diff)
+	}
+}
+
+func TestTeamsService_CreateTeamMember(t *testing.T) {
+	t.Parallel()
+
+	server, client, teardown := newMockServerAndClient(t)
+	t.Cleanup(teardown)
+
+	server.HandleFunc("/v2/organizations/my-great-org/teams/123/members", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		fmt.Fprint(w, `{"user_id":"456"}`)
+	})
+
+	createTeamMember := CreateTeamMember{UserID: "456"}
+	m, _, err := client.Teams.CreateTeamMember(context.Background(), "my-great-org", "123", createTeamMember)
+	if err != nil {
+		t.Errorf("Teams.CreateTeamMember returned error: %v", err)
+	}
+
+	want := TeamMember{ID: "456"}
+	if diff := cmp.Diff(m, want); diff != "" {
+		t.Errorf("Teams.CreateTeamMember diff: (-got +want)\n%s", diff)
+	}
+}
+
+func TestTeamsService_UpdateTeamMember(t *testing.T) {
+	t.Parallel()
+
+	server, client, teardown := newMockServerAndClient(t)
+	t.Cleanup(teardown)
+
+	server.HandleFunc("/v2/organizations/my-great-org/teams/123/members/456", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "PATCH")
+		fmt.Fprint(w, `{"user_id":"456"}`)
+	})
+
+	m, _, err := client.Teams.UpdateTeamMember(context.Background(), "my-great-org", "123", "456", "maintainer")
+	if err != nil {
+		t.Errorf("Teams.UpdateTeamMember returned error: %v", err)
+	}
+
+	want := TeamMember{ID: "456"}
+	if diff := cmp.Diff(m, want); diff != "" {
+		t.Errorf("Teams.UpdateTeamMember diff: (-got +want)\n%s", diff)
+	}
+}
+
+func TestTeamsService_DeleteTeamMember(t *testing.T) {
+	t.Parallel()
+
+	server, client, teardown := newMockServerAndClient(t)
+	t.Cleanup(teardown)
+
+	server.HandleFunc("/v2/organizations/my-great-org/teams/123/members/456", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "DELETE")
+	})
+
+	_, err := client.Teams.DeleteTeamMember(context.Background(), "my-great-org", "123", "456")
+	if err != nil {
+		t.Errorf("Teams.DeleteTeamMember returned error: %v", err)
+	}
+}
