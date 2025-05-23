61840b79f1c68e8b72e816c8bc6d08ec3fb8b4bc
Ben McNicholl <git@benmcnicholl.com>
2025-05-23 10:22:24 +1000

    chore(fmt): format new version file
    Signed-off-by: Ben McNicholl <git@benmcnicholl.com>

--- version.go
+++ version.go
@@ -12,20 +12,20 @@ var Version = func() string {
 	if !ok {
 		return "dev" // No build info available
 	}
-	
+
 	// For the main module
 	if info.Main.Path == "github.com/buildkite/go-buildkite/v4" && info.Main.Version != "(devel)" {
 		// Remove the 'v' prefix if present
 		return strings.TrimPrefix(info.Main.Version, "v")
 	}
-	
+
 	// Try to find this module in the dependency list
 	for _, dep := range info.Deps {
 		if dep.Path == "github.com/buildkite/go-buildkite/v4" && dep.Version != "(devel)" {
 			return strings.TrimPrefix(dep.Version, "v")
 		}
 	}
-	
+
 	// If we're in development mode, try to get VCS info
 	for _, setting := range info.Settings {
 		if setting.Key == "vcs.revision" {
@@ -36,7 +36,7 @@ var Version = func() string {
 			return "dev+" + setting.Value
 		}
 	}
-	
+
 	// Last resort fallback
 	return "dev"
 }()
