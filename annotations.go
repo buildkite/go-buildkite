30b9bc9cf9a99bc537acae54a25706203f73ddb4
Jeremy Bumsted <jeremy@Jeremys-MacBook-Pro-2.local>
2026-04-17 11:26:01 -0600

    feat: Add job-level annotations API support
      The Buildkite API now supports annotations scoped to individual jobs.
      Adds ListByJob, CreateForJob, and DeleteForJob methods to AnnotationsService,
      and extends the Annotation and AnnotationCreate structs with the new scope
      and priority fields present on both build and job annotation responses.
    
     Committer: Jeremy Bumsted <jeremy@buildkite.com>

--- annotations.go
+++ annotations.go
@@ -19,16 +19,19 @@ type Annotation struct {
 	ID        string     `json:"id,omitempty"`
 	Context   string     `json:"context,omitempty"`
 	Style     string     `json:"style,omitempty"`
+	Scope     string     `json:"scope,omitempty"`
+	Priority  int        `json:"priority,omitempty"`
 	BodyHTML  string     `json:"body_html,omitempty"`
 	CreatedAt *Timestamp `json:"created_at,omitempty"`
 	UpdatedAt *Timestamp `json:"updated_at,omitempty"`
 }
 
 type AnnotationCreate struct {
-	Body    string `json:"body,omitempty"`
-	Context string `json:"context,omitempty"`
-	Style   string `json:"style,omitempty"`
-	Append  bool   `json:"append"`
+	Body     string `json:"body,omitempty"`
+	Context  string `json:"context,omitempty"`
+	Style    string `json:"style,omitempty"`
+	Priority int    `json:"priority,omitempty"`
+	Append   bool   `json:"append"`
 }
 
 // AnnotationListOptions specifies the optional parameters to the
@@ -78,6 +81,61 @@ func (as *AnnotationsService) Create(ctx context.Context, org, pipeline, build s
 
 func (as *AnnotationsService) Delete(ctx context.Context, org, pipeline, build, annotationUUID string) (*Response, error) {
 	u := fmt.Sprintf("v2/organizations/%s/pipelines/%s/builds/%s/annotations/%s", org, pipeline, build, annotationUUID)
+	req, err := as.client.NewRequest(ctx, http.MethodDelete, u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return as.client.Do(req, nil)
+}
+
+// ListByJob gets annotations for a specific job
+//
+// buildkite API docs: https://buildkite.com/docs/apis/rest-api/annotations#list-annotations-for-a-job
+func (as *AnnotationsService) ListByJob(ctx context.Context, org, pipeline, build, jobID string, opt *AnnotationListOptions) ([]Annotation, *Response, error) {
+	u := fmt.Sprintf("v2/organizations/%s/pipelines/%s/builds/%s/jobs/%s/annotations", org, pipeline, build, jobID)
+	u, err := addOptions(u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := as.client.NewRequest(ctx, "GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var annotations []Annotation
+	resp, err := as.client.Do(req, &annotations)
+	if err != nil {
+		return nil, resp, err
+	}
+	return annotations, resp, err
+}
+
+// CreateForJob creates an annotation scoped to a specific job
+//
+// buildkite API docs: https://buildkite.com/docs/apis/rest-api/annotations#create-an-annotation-on-a-job
+func (as *AnnotationsService) CreateForJob(ctx context.Context, org, pipeline, build, jobID string, ac AnnotationCreate) (Annotation, *Response, error) {
+	u := fmt.Sprintf("v2/organizations/%s/pipelines/%s/builds/%s/jobs/%s/annotations", org, pipeline, build, jobID)
+	req, err := as.client.NewRequest(ctx, "POST", u, ac)
+	if err != nil {
+		return Annotation{}, nil, err
+	}
+
+	var annotation Annotation
+	resp, err := as.client.Do(req, &annotation)
+	if err != nil {
+		return Annotation{}, resp, err
+	}
+
+	return annotation, resp, err
+}
+
+// DeleteForJob deletes an annotation on a job
+//
+// buildkite API docs: https://buildkite.com/docs/apis/rest-api/annotations#delete-an-annotation-on-a-job
+func (as *AnnotationsService) DeleteForJob(ctx context.Context, org, pipeline, build, jobID, annotationUUID string) (*Response, error) {
+	u := fmt.Sprintf("v2/organizations/%s/pipelines/%s/builds/%s/jobs/%s/annotations/%s", org, pipeline, build, jobID, annotationUUID)
 	req, err := as.client.NewRequest(ctx, http.MethodDelete, u, nil)
 	if err != nil {
 		return nil, err
---
+++ examples/annotations/create_for_job/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/buildkite/go-buildkite/v4"
+
+	"github.com/alecthomas/kingpin/v2"
+)
+
+var (
+	apiToken = kingpin.Flag("token", "API token").Required().String()
+	org      = kingpin.Flag("org", "Orginization slug").Required().String()
+	slug     = kingpin.Flag("slug", "Pipeline slug").Required().String()
+	number   = kingpin.Flag("number", "Build number").Required().String()
+	jobID    = kingpin.Flag("job", "Job ID").Required().String()
+)
+
+func main() {
+	kingpin.Parse()
+
+	client, err := buildkite.NewOpts(buildkite.WithTokenAuth(*apiToken))
+	if err != nil {
+		log.Fatalf("creating buildkite API client failed: %v", err)
+	}
+
+	annotationCreate := buildkite.AnnotationCreate{
+		Style:   "info",
+		Context: "default",
+		Body:    "An example job annotation!",
+		Append:  false,
+	}
+
+	annotation, _, err := client.Annotations.CreateForJob(context.Background(), *org, *slug, *number, *jobID, annotationCreate)
+	if err != nil {
+		log.Fatalf("Creating annotation for job %s in build %s failed: %s", *jobID, *number, err)
+	}
+
+	data, err := json.MarshalIndent(annotation, "", "\t")
+	if err != nil {
+		log.Fatalf("json encode failed: %s", err)
+	}
+
+	_, _ = fmt.Fprintf(os.Stdout, "%s", string(data))
+}
